4759ea4d4a7e6b9d1b222b458d2a48ca14e084d5
s3thy <sethy.chea@googlemail.com>
2018-05-10 16:20:39 +0200

    irgendwas ist immer

--- src/main/java/controller/login/Funktionen.java
+++ src/main/java/controller/login/Funktionen.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import controller.ViewController;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -17,55 +15,25 @@ import static app.Main.primaryStage;
 public class Funktionen
 {
    int x = 0;
-   // int y = 0;
 
    public void twerkStage()
    {
-      Timeline timelineX = new Timeline(new KeyFrame(Duration.seconds(0.05), new EventHandler<ActionEvent>()
-      {
-         @Override
-         public void handle(ActionEvent t)
+      Timeline timelineX = new Timeline(new KeyFrame(Duration.seconds(0.05), t -> {
+         if( x == 0 )
          {
-            if( x == 0 )
-            {
-               primaryStage.setX(primaryStage.getX() + 10);
-               x = 1;
-            }
-            else
-            {
-               primaryStage.setX(primaryStage.getX() - 10);
-               x = 0;
-            }
+            primaryStage.setX(primaryStage.getX() + 10);
+            x = 1;
+         }
+         else
+         {
+            primaryStage.setX(primaryStage.getX() - 10);
+            x = 0;
          }
       }));
 
       timelineX.setCycleCount(5);
       timelineX.setAutoReverse(false);
       timelineX.play();
-
-/*
-      Timeline timelineY = new Timeline(new KeyFrame(Duration.seconds(0.1), new EventHandler<ActionEvent>()
-      {
-         @Override
-         public void handle(ActionEvent t)
-         {
-            if( y == 0 )
-            {
-               primaryStage.setY(primaryStage.getY() + 10);
-               y = 1;
-            }
-            else
-            {
-               primaryStage.setY(primaryStage.getY() - 10);
-               y = 0;
-            }
-         }
-      }));
-
-      timelineY.setCycleCount(Timeline.INDEFINITE);
-      timelineY.setAutoReverse(false);
-      timelineY.play();
-*/
    }
 
    public void logMeInOrNot(String txt_pwd, String txt_name)
@@ -109,7 +77,6 @@ public class Funktionen
          alert.setContentText("Name und/oder Passwort nicht korrekt");
          alert.showAndWait();
          */
-
          twerkStage();
       }
    }
